313968eeedabdf481a4134300e2dfde952385d09
zzzNathan <132711950+zzzNathan@users.noreply.github.com>
2023-12-17 11:41:58 +0000

    Add files via upload

---
+++ Todo.py
@@ -0,0 +1,51 @@
+# To-do list
+# ----------
+import datetime, sys
+TASKS_FILE = r'C:\Users\patyt\Task.txt'
+
+def AddTask():
+  # Get task and deadline
+  label    = input('What is this task called?')
+  deadline = input('When is this task due?')
+
+  # Add task to the file
+  file = open(TASKS_FILE,'a')
+  file.write(f'\n  - {label} | Due: {deadline}')
+
+  # Close the file
+  file.close()
+
+# Prints current date and time
+print( str(datetime.datetime.now())[:-10] )
+print(
+'''
+#/*******************************************\ \n
+#                   T A S K S                  \n
+#                  - - - - - -                 \n
+#\*******************************************/''' )
+
+mode = None
+while mode != 'q':
+  print('\n\nYou may press \'q\' to quit')
+  mode = input('Type \'v\' to view tasks and \'w\' to enter a new task')
+
+  match mode:
+    # Reads current tasks from file
+    case 'v':
+      file = open(TASKS_FILE,'r')
+      print('------------------------------------\n')
+      print( file.read() )
+      file.close()
+      
+    # Adds task to file
+    case 'w':AddTask()
+    
+    # Quit application
+    case 'q':break
+    
+    # Handles misinputs
+    case other:print('Command not recognised')
+
+  print('\n\n\n')
+  
+print('Application was quit!')
